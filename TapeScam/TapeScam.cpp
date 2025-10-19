15f519e71bfc8b4558667da0f9675e8bf50e36fc
Charles Vestal <charles@charles.pizza>
2025-10-19 10:01:32 +0200

    Apply output level only when effect engaged

--- TapeScam/TapeScam.cpp
+++ TapeScam/TapeScam.cpp
@@ -77,15 +77,26 @@ void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, s
     }
 
     const float levelKnob = hw.GetKnobValue(Hothouse::KNOB_6);
-    const float minLevelDb = -36.0f;
-    const float levelDb   = minLevelDb + levelKnob * (-minLevelDb);
-    globalLevelTarget = powf(10.0f, levelDb / 20.0f);
+    if(levelKnob <= 0.0005f)
+    {
+        globalLevelTarget = 0.0f;
+    }
+    else
+    {
+        const float minLevelDb = -60.0f;
+        const float levelDb   = minLevelDb + levelKnob * (-minLevelDb);
+        globalLevelTarget = powf(10.0f, levelDb / 20.0f);
+    }
+
     globalLevelCurrent += (globalLevelTarget - globalLevelCurrent) * kLevelSmooth;
 
-    for(size_t i = 0; i < size; ++i)
+    if(!bypassEnabled)
     {
-        out[0][i] = ClampLevel(out[0][i] * globalLevelCurrent, -1.0f, 1.0f);
-        out[1][i] = ClampLevel(out[1][i] * globalLevelCurrent, -1.0f, 1.0f);
+        for(size_t i = 0; i < size; ++i)
+        {
+            out[0][i] = ClampLevel(out[0][i] * globalLevelCurrent, -1.0f, 1.0f);
+            out[1][i] = ClampLevel(out[1][i] * globalLevelCurrent, -1.0f, 1.0f);
+        }
     }
 }
 
